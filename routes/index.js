f110b2e59deb1214978fd78275005392f5ea1dc0
johnkuefler <john@dev-squared.com>
2023-11-02 18:36:44 -0500

    Updates

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  let userData = await axios.get('https://jsonplaceholder.typicode.com/users');
+
   res.render('index', 
     { 
+      userData: userData.data,
       title: 'Happy Haloween',
-      content: 'It is a great day'
+      content: 'It is a great day',
+      isRaining: true,
+      dailyForecast: [
+        {day: 'Monday', temp: 45 },
+        {day: 'Tuesday', temp: 50 },
+        {day: 'Wednesday', temp: 30 },
+        {day: 'Thursday', temp: 55 },
+        {day: 'Friday', temp: 60 },
+        {day: 'Saturday', temp: 70 },
+        {day: 'Sunday', temp: 45 },
+      ]
     });
 });
 
