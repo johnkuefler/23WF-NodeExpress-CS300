7529d5bb0faf5163bc7005b6e7fbe0e6b548fec6
johnkuefler <john@dev-squared.com>
2023-11-02 08:07:44 -0500

    Updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Happy Haloween' });
+  res.render('index', 
+    { 
+      title: 'Happy Haloween',
+      content: 'It is a great day'
+    });
 });
 
 router.get('/test', function(req, res, next) {
